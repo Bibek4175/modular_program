8af050ab85243c9d1dc0e8104471087c9af38798
root <chandbibak09@gmail.com>
2024-06-29 11:02:10 +0545

    Dummy Project

---
+++ venv/board/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from board import pages
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.register_blueprint(pages.bp)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+
---
+++ venv/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='board',
+   version='1.0',
+   description='A simple dummy project',
+   author = 'abc',
+   packages = ['board'],
+   install_requires=['flask','numpy'],
+)
